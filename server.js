bcc0688a7ec17cd6c5fe809400069a13d465c4d3
TUNGVU <tvu727531@gmail.com>
2026-07-03 15:31:12 +0700

    Update HTML title and heading content

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-
+// 1. TRANG XEM WEB CÔNG KHAI (BẢN CẬP NHẬT GAY GẮT)
 app.get('/', (req, res) => {
     res.send(`
         <html lang="vi">
         <head>
             <meta charset="UTF-8">
-            <title> </title>
+            <title>Gửi mấy thằng ăn cắp</title>
             <style>
                 body { 
                     margin: 0; 
@@ -17,16 +17,17 @@ app.get('/', (req, res) => {
                 }
                 h1 { 
                     color: #ff0000; 
-                    font-size: 5rem; 
+                    font-size: 4rem; 
                     text-align: center; 
                     text-transform: uppercase; 
                     text-shadow: 0 0 20px #ff0000;
                     padding: 20px;
+                    line-height: 1.5;
                 }
             </style>
         </head>
         <body>
-            <h1>gọi mấy thk skip qua nỗi chuyện với tao</h1>
+            <h1>tắt likes cho ah tày tao likes tắt likes cho anh tboy tao like gọi mấy thk skips qua nói chuyện với tao</h1>
         </body>
         </html>
     `);
